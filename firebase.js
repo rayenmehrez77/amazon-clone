2add88ed08094d509d22b48a1b2118fcc6c7620a
Rayen Mehrez <54637377+rayenmehrez77@users.noreply.github.com>
2021-05-23 01:20:14 +0200

    Completed authentication with firebase and nextAurth, completed header routing, working on checkout page

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3_AOtgGOep80NbDtgIhLtPYGSU4HfDxg",
+  authDomain: "thenext-45034.firebaseapp.com",
+  projectId: "thenext-45034",
+  storageBucket: "thenext-45034.appspot.com",
+  messagingSenderId: "344483591026",
+  appId: "1:344483591026:web:c8d634fc2991bd4f9ce3eb",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Image from "next/image";
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from "@heroicons/react/solid";
+import { useSession, signIn, signOut } from "next-auth/client";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const [session] = useSession();
+  const router = useRouter();
+
+  console.log(router.pathname);
+
   return (
     <header>
       {/* top nav */}
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -24,15 +32,20 @@ const Header = () => {
           <SearchIcon className="h-12 p-4" />
         </div>
         <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div className="link">
-            <p>Hello, Rayen Mehrez</p>
+          <div className="link" onClick={!session ? signIn : signOut}>
+            <p className="hover:underline">
+              {session ? `Hello, ${session.user.name}` : "Sign In"}
+            </p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
           <div className="link">
             <p>Returns </p>
             <p className="font-extrabold md:text-sm"> & Orders</p>
           </div>
-          <div className="relative link flex items-center">
+          <div
+            className="relative link flex items-center"
+            onClick={() => router.push("/checkout")}
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
               0
             </span>
