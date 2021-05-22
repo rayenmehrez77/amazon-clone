61d8ef4ac9aa1b278169e116520244aa9c46129a
Rayen Mehrez <54637377+rayenmehrez77@users.noreply.github.com>
2021-05-22 21:59:18 +0200

    completed the main container

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -12,6 +14,24 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* ---- ---- */}
+      <main className="max-w-screen-2xl mx-auto	">
+        {/* Banner */}
+        <Banner />
+        {/* ProductFeed */}
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  const products = await fetch("https://fakestoreapi.com/Products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
